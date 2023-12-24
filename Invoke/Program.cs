6e3fc32bf0862082bfc2f9927b0ada64edead693
Patrick Souza <patrickaquinodesouza@gmail.com>
2023-12-24 14:53:53 -0300

    template lambda

---
+++ Invoke/Program.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.Lambda;
+using Amazon.Lambda.Model;
+using Newtonsoft.Json;
+
+namespace client
+{
+    class Program
+    {
+        static AmazonLambdaClient Client = new AmazonLambdaClient();
+        static void Main(string[] args)
+        {
+            string input = "\"message\"";
+
+            var request = new InvokeRequest()
+            {
+                FunctionName = "DefaultLambda",
+                Payload = input
+            };
+
+            var response = Client.InvokeAsync(request).Result;
+
+            Console.WriteLine($"Result => { new StreamReader(response.Payload).ReadToEnd() }");
+        }
+    }
+}
